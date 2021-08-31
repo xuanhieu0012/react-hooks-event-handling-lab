9a3d91481fa72bbf807e7071562b7fd4ac1d5827
Hieu <xuanhieu0012@gmail.com>
2021-08-31 16:30:52 -0500

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function HandleBlur(){
+    console.log("Hey! Eyes on me!")
+}
+function HandleFocus(){
+    console.log("Good!")
+
+}
+
+function EyesOnMe(){
+    
+    
+    return(
+        <button onFocus={HandleFocus} onBlur={HandleBlur}>Eyes on Me</button>
+    );
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from "react"
+
+function handleChange(){
+    console.log("Entering password...")
+}
+
+function Keypad(){
+    return (
+    <input type="password" 
+    placeholder ="Enter Password"
+    onChange={handleChange}
+    />
+    )
+}
+
+export default Keypad
